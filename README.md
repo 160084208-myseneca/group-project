7f458c443c9582eb2381d5c323c26fafc1bf5990
dwsalas <dwsalas@myseneca.ca>
2023-03-25 23:45:48 -0400

    adds rpsGame.py in branch and created branch

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# group-project
+# Group Project 
+
+ **COURSE INFORMATION: OPS445-NBB**
+
+ **GROUPS’S NAME: Don Werner Salas, Alwen Roselio, Carl Fajardo**
+
+ **GROUPS'S STUDENT NUMBER: DON-160084208,ALWEN-160085205**
+
+ **GITHUB GROUPS USER_ID: 160084208-MySeneca,160085205-myseneca,** 
+
+ **TEACHER’S NAME: Ben Singh**
+
+---
+ ## Table Of contents 
+1. [Plan](#Image)
+2. [Game Name](#Single-Line-Code-Example)
+3. [How this Game works](#Multi-Line-Code-Example)
+4. [Commands Used](#Json-Objects)
+
+---
+## Plan
+
+
+---
+## Game Name
+
+---
+
+## Hows This Game Works
+
+---
+
+## Commands Used
+
+| COMMANDS USED  | SCREENSHOT     |
+| -------------- | --------------|
+| `git clone git clone https://github.com/160084208-myseneca/Assignment-1.git` | [screenshot1](./images/grouprespository.JPG) |
+| `git branch Iteration1 ` | [screenshot2](./images/gitbranch.JPG) |
+| `git add .` | ![screenshot3](screenshots/3.png) |
+| `git commit -m "Commit message"` | ![screenshot4](screenshots/4.png) |
+| `git push origin new_branch` | ![screenshot5](screenshots/5.png) |
+| `git pull origin main` | ![screenshot6](screenshots/6.png) |
+| `git merge feature_branch` | ![screenshot7](screenshots/7.png) |
+| `git log` | ![screenshot8](screenshots/8.png) |
+| `git status` | ![screenshot9](screenshots/9.png) |
+| `git branch` | ![screenshot10](screenshots/10.png) |
---
+++ rpsGAME.py
@@ -0,0 +1,50 @@
+import random
+
+# List of valid options
+options = ["rock", "paper", "scissors"]
+
+print("Welcome to Rock-Paper-Scissors!")
+
+# Loop for multiple rounds
+while True:
+    # Computer's random choice
+    computer_option = random.choice(options)
+
+    # User input
+    user_option = input("Enter your choice (rock/paper/scissors): ").lower()
+
+    # Check if user input is valid
+    if user_option not in options:
+        print("Invalid choice. Please try again.")
+        continue
+
+    # Print user and computer options
+    print(f"\nYou chose {user_option}.")
+    print(f"The computer chose {computer_option}.\n")
+
+    # Determine winner
+    if user_option == computer_option:
+        print("It's a tie!")
+    elif user_option == "rock":
+        if computer_option == "paper":
+            print("Computer wins!")
+        else:
+            print("You win!")
+    elif user_option == "paper":
+        if computer_option == "scissors":
+            print("Computer wins!")
+        else:
+            print("You win!")
+    else:
+        if computer_option == "rock":
+            print("Computer wins!")
+        else:
+            print("You win!")
+
+    # Ask user to play again
+    play_again = input("Do you want to play again? (yes or no): ").lower()
+    if play_again == "no":
+        break
+
+# End of game
+print("Thanks for playing!")
